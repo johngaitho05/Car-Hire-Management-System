924a6987db9c1fd3de11f6188bbb9efa8b2f2f75
johngaitho05 <johngaitho05@gmail.com>
2020-07-09 15:00:41 +0300

    Added info cards

--- home.php
+++ home.php
@@ -20,11 +20,41 @@ and open the template in the editor.
     // Include config file
     require_once "navbar.php";
    ?>
- 
+        
         <div class="wrapper">
-            <div class="info-card"></div>
-            <div class="info-card"></div>
-            <div class="info-card"></div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <h4 class="page-title">Dashboard</h4>
+                </div>
+                <div class="col-sm-3">
+                    <div class="info-card hired">
+                        <div class="info-card-icon"><i class="fas fa-times-circle"></i></div>
+                        <p class="info-card-heading">Hired </p>
+                        <div class="info-card-amount">68</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="info-card available">
+                        <div class="info-card-icon"><i class="fas fa-check-circle"></i></div>
+                        <p class="info-card-heading">Available</p>
+                        <div class="info-card-amount">112</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="info-card earnings">
+                        <div class="info-card-icon"><i class="fas fa-calendar"></i></div>
+                        <p class="info-card-heading">Earnings (Monthly)</p>
+                        <div class="info-card-amount"><i class="fas fa-dollar-sign"></i>3500</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="info-card served">
+                        <div class="info-card-icon"><i class="fas fa-user-check"></i></div>
+                        <p class="info-card-heading">Clients Served</p>
+                        <div class="info-card-amount">120</div>
+                    </div>
+                </div>
+            </div>
         </div>
         <script type="text/javascript" src="js/jquery-3.3.1.js"></script>
         <script type="text/javascript" src="js/bootstrap.js"></script> 
